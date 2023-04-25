7013f101270b3ec9e51e307f53994d1e85eff3ea
nikhilesh-mittal <nikhileshmittal28@gmail.com>
2023-04-25 16:39:13 +0530

    Building Navbar and folder structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Paragraph from "@/components/ui/Paragraph";
 
 export default function Home() {
-  return <main className={"bg-red-500"}>Hello World</main>;
+  return (
+    <main className={"bg-red-500"}>
+    </main>
+  );
 }
