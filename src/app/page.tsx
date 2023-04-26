6ab1db0760fa7d7d0e83f8060b38357b94c16fa0
nikhilesh-mittal <nikhileshmittal28@gmail.com>
2023-04-26 12:10:31 +0530

    Home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,53 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+// import { Inter } from 'next/font/google'
+
+import LargeHeading from "@/components/ui/LargeHeading";
 import Paragraph from "@/components/ui/Paragraph";
+import type { Metadata } from "next";
+import Image from "next/image";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "Similarity API | Home",
+  description: "Free & open-source text similarity API",
+};
 
 export default function Home() {
   return (
-    <main className={"bg-red-500"}>
-    </main>
+    <div className="relative h-screen flex items-center justify-center overflow-x-hidden">
+      <div className="container pt-32 max-w-7xl mx-auto w-full h-full">
+        <div className="h-full gap-6 flex flex-col justify-start lg:justify-center items-center lg:items-start">
+          <LargeHeading
+            size="lg"
+            className="three-d text-black dark:text-light-gold"
+          >
+            Easily determine <br /> text similarity.
+          </LargeHeading>
+
+          <Paragraph className="max-w-xl lg:text-left">
+            With the text similarity API, you can easily determine the
+            similarity between two pieces of text with a free{" "}
+            <Link
+              href={"/login"}
+              className="underline underline-offset-2 text-black dark:text-light-gold"
+            >
+              API Key
+            </Link>
+            .
+          </Paragraph>
+
+          <div className="relative w-full max-w-lg lg:max-w-3xl lg:left-1/2 aspect-square lg:absolute ">
+            <Image
+              alt="typewriter"
+              priority
+              className="img-shadow"
+              quality={100}
+              style={{ objectFit: "contain" }}
+              fill
+              src="/typewriter.png"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
--- src/components/ui/Paragraph.tsx
+++ src/components/ui/Paragraph.tsx
@@ -3,7 +3,7 @@ import { VariantProps, cva } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const paragraphVariants = cva(
-  "max-w-prose text-slate-700 dark:test-slate-300 mb-2 text-center",
+  "max-w-prose text-slate-700 dark:test-slate-100 dark:text-white mb-2 text-center",
   {
     variants: {
       size: {
